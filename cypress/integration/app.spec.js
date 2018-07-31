437c5cacec4d5493dc3b600cddd8cd9b628deb89
Ashton K Ellis <ashtonkellis@gmail.com>
2018-07-30 22:16:00 -0700

    cypres tests done

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,48 @@
+describe('Testing budget tracker app', () => {
+  it('Should create a new category and a new expense and delete them', () => {
+    expect(true).to.equal(true);
+    
+    cy.visit('http://localhost:8080');
+
+    cy.get('form[data-cy=category-form] input[type=text]')
+      .type('Test Category 1');
+    
+    cy.get('form[data-cy=category-form] input[type=number]')
+      .clear()
+      .type('100')
+    
+    cy.get('form[data-cy=category-form]')
+      .submit();
+
+    cy.get('[data-cy=category]')
+      .then((categories) => {
+        expect(categories).to.have.length(1);
+      })
+
+    cy.get('form[data-cy=expense-form] input[type=text]')
+      .type('Test Expense 1');
+
+    cy.get('form[data-cy=expense-form] input[type=number]')
+      .clear()
+      .type('50');
+
+    cy.get('form[data-cy=expense-form]').submit();
+
+    cy.get('[data-cy=expense]')
+      .then((expense) => {
+        expect(expense).to.have.length(1);
+      })
+
+    cy.get('[data-cy=category-delete-button]').click()
+
+    cy.get('[data-cy=category]')
+      .then((categories) => {
+        expect(categories).to.have.length(0);
+      })
+    
+      cy.get('[data-cy=expense]')
+        .then((expenses) => {
+          expect(expenses).to.have.length(0);
+        });
+  });
+});
--- src/components/category-form/category-form.js
+++ src/components/category-form/category-form.js
@@ -26,6 +26,7 @@ export default class CategoryForm extends React.Component {
     const buttonText = this.props.category ? 'Update' : 'Create';
     return (
       <form
+        data-cy="category-form"
         onSubmit={ this.handleSubmit }
         className="category-form"
       >
